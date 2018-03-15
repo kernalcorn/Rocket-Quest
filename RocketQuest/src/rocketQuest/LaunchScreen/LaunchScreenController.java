904bbf16f2d85a90da73597c75a2476871115d93
tehfa <tehfa@LAPTOP-5L1A7J99.fcs.org>
2018-03-15 14:16:49 -0400

    CODEEEEE

---
+++ RocketQuest/src/rocketQuest/LaunchScreen/LaunchScreenController.java
@@ -0,0 +1,21 @@
+package rocketQuest.LaunchScreen;
+
+import rocketQuest.Main;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class LaunchScreenController {
+
+	private Main mainApp;
+	
+	public LaunchScreenController()
+	{
+		
+	}
+	
+	public void setMainApp(Main mainApp) 
+	{
+		this.mainApp = mainApp;
+	}
+}
---
+++ RocketQuest/src/rocketQuest/Workshop/Workshop.java
@@ -0,0 +1,5 @@
+package rocketQuest.Workshop;
+
+public class Workshop {
+
+}
