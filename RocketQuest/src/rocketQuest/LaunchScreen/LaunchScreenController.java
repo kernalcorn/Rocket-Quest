7451d7b6ecda3ad2333a8401c09963e2f1a31408
tehfa <tehfa@LAPTOP-5L1A7J99.fcs.org>
2018-03-19 14:15:15 -0400

    fx

--- RocketQuest/src/rocketQuest/LaunchScreen/LaunchScreenController.java
+++ RocketQuest/src/rocketQuest/LaunchScreen/LaunchScreenController.java
@@ -1,6 +1,9 @@
 package rocketQuest.LaunchScreen;
 
 import rocketQuest.Main;
+
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,6 +15,12 @@ public class LaunchScreenController {
 	public LaunchScreenController()
 	{
 		
+	}
+	
+	@FXML
+	public void handleWorkshopSwitch() throws IOException
+	{
+		Main.showWorkshopScreen();
 	}
 	
 	public void setMainApp(Main mainApp) 
